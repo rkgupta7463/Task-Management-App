f114e69d11416eceeb320822f426ea0294d31ec7
Rishu Gupta <rishukumargupta8409@gmail.com>
2026-01-17 00:14:29 +0530

    change update

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-from db.schema import userSchemas
-from db.database import SessionLocal, engine
+from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from db.models import userModel
 from utils.init_db import create_tables
 from router.auth import authRouter
-from router.usersRoute import create_user
 from router.taskRoute import taskRouter
 from router.categoryRoute import categoryRouter
-from db.database import get_db
-from utils.protectedRoute import get_current_user
-from db.schema.userSchemas import UserResponse
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
